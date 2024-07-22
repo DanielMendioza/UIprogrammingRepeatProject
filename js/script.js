4dd7a63cb3b962505ea00ed77b813169a61f6212
danielmendioza <c00274269@itcarlow.ie>
2024-07-22 23:02:49 +0100

    D-Pad Buttons added

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
+
+let yellowButton = document.getElementsByClassName("YellowBtn")[0];
+let blueButton = document.getElementsByClassName("BlueBtn")[0];
+let redButton = document.getElementsByClassName("RedBtn")[0];
+let greenButton = document.getElementsByClassName("GreenBtn")[0];
