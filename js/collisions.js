bc73acaaf4e0c118bab4676e78fa0d8f0e406f14
danielmendioza <c00274269@itcarlow.ie>
2024-07-23 16:39:15 +0100

    Refactored Collision blocks

--- js/collisions.js
+++ js/collisions.js
@@ -9,6 +9,21 @@ const colissionlevel1 =
 ,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0
 ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];//layout of the main level
 
+class Collisionblock{
+    constructor({position}){
+        this.position = position
+        this.width = 64
+        this.height = 64
+    }
+    draw(){
+        ctx.fillStyle = `rgba(128,128,128,0.5)`;
+        ctx.fillRect(this.position.x, 
+                     this.position.y,
+                     this.width,
+                     this.height)
+    }
+}
+
 Array.prototype.parse2D = function(){
     const rows = [];
     //This loops through the array in steps of 16, the amount of columns
@@ -27,38 +42,32 @@ Array.prototype.parse2D = function(){
     return rows;
 }
 
+
+Array.prototype.CreateObjectsFrom2d = function(){
+    const objects = [];//empty array to store 
+    this.forEach((row, columnHeight)=>{//same function to chop the array into pieces and create a 2darray
+        row.forEach((symbol, rowWidth) => {
+            if (symbol === 1) {
+                objects.push(new Collisionblock({
+                    position: {
+                        x: rowWidth * 64,
+                        y: columnHeight * 64,
+                    }
+                }))
+            }
+        })
+    })
+
+    return objects;
+}
+
+let collisionBlocks = [];
 // Parse the collision level array into a 2D array
 const parsedCollisions = colissionlevel1.parse2D()
 
-class Collisionblock{
-    constructor({position}){
-        this.position = position
-        this.width = 64
-        this.height = 64
-    }
-    draw(){
-        ctx.fillStyle = `rgba(128,128,128,0.5)`;
-        ctx.fillRect(this.position.x, 
-                     this.position.y,
-                     this.width,
-                     this.height)
-    }
-}
+collisionBlocks = parsedCollisions.CreateObjectsFrom2d();
 
-// Initialized an empty array to hold all collision blocks
-const collisionBlocks = []
+
 
 //console.log(parsedCollisions)  just an output to see if it prints on console
 
-parsedCollisions.forEach((row, columnHeight)=>{
-    row.forEach((symbol, rowWidth) => {
-        if (symbol === 1) {// If the symbol is 1, create a new CollisionBlock at the corresponding position
-            collisionBlocks.push(new Collisionblock({
-                position: {
-                    x: rowWidth * 64,
-                    y: columnHeight * 64,
-                }
-            }))
-        }
-    })
-})
